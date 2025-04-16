60e9e855b15edd72a803487330be5bbc71f35e52
Emmanuele Levi <ignisvictorious@gmail.com>
2025-04-16 11:19:30 +0100

    commit files left out

---
+++ src/band_db.py
@@ -0,0 +1,13 @@
+#  ________________
+#  Import LIBRARIES
+#  Import FILES
+#  ________________
+
+
+BANDS: list[dict] = [
+    {"id": 1, "name": "The Kinks", "genre": "Rock"},
+    {"id": 2, "name": "Aphex Twin", "genre": "Electronic"},
+    {"id": 3, "name": "Slowdive", "genre": "Shoegaze"},
+    {"id": 4, "name": "Wu-Tang Clan", "genre": "Hip-Hop"},
+    {"id": 4, "name": "Added One of My Own", "genre": "Classic"},
+]
---
+++ src/schema.py
@@ -0,0 +1,12 @@
+#  ________________
+#  Import LIBRARIES
+from enum import Enum
+#  Import FILES
+#  ________________
+
+
+class GenreURLChoices(Enum):
+    ROCK = "rock"
+    ELECTRONIC = "electronic"
+    METAL = "metal"
+    HIP_HOP = "hip-hop"
